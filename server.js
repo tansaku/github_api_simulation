77c855eb4ac851b926e9c0d1d38ca88ecff98645
Sam Joseph <tansaku@gmail.com>
2015-02-05 07:39:56 +0000

    first homepage test passing

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var port = 9999;
 
+app.get('/', function(request, response){
+  response.send('Github API Simulation');
+});
+
 server.listen(port, function(){
   console.log('server listening on port: ' + port);
 });
