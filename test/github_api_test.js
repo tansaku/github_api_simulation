e83734eda335c22cfcec0dc75b25942124147bf2
Sam Joseph <tansaku@gmail.com>
2015-02-05 07:27:59 +0000

    trivial first test pass with chai

--- test/github_api_test.js
+++ test/github_api_test.js
@@ -1,5 +1,8 @@
+var chai = require('chai');
+var expect = chai.expect;
+
 describe('Github API Simulation', function(){
   it('should return user data in json format', function(){
-    fail();
+    expect(2+2).to.equal(4);
   });
 });
