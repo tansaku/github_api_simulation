f7f6f7fb07d04ff3ec07541dc15b74b8e107b35e
Sam Joseph <tansaku@gmail.com>
2015-02-05 10:48:48 +0000

    updating with more data

--- test/github_api_test.js
+++ test/github_api_test.js
@@ -18,14 +18,14 @@ describe('Github API Simulation', function(){
   it('should return JSON for a particular user', function(){
     casper.thenOpen(host + '/users/tansaku', function(response){
       expect(response.headers.get('Access-Control-Allow-Origin')).to.equal('*');
-      expect('body').to.have.text('{"login":"tansaku", "followers":"12"}');
+      expect('body').to.have.text('{"login":"tansaku", "html_url":"147", "public_repos":"216", "public_followers":"147", "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3"}');
     });
   });
 
   it('should return JSON for a another user', function(){
     casper.thenOpen(host + '/users/henrygarner', function(response){
       expect(response.headers.get('Access-Control-Allow-Origin')).to.equal('*');
-      expect('body').to.have.text('{"login":"henrygarner", "followers":"15"}');
+      expect('body').to.have.text('{"login":"henrygarner", "html_url":"24", "public_repos":"77", "public_followers":"24", "avatar_url": "https://avatars.githubusercontent.com/u/24540?v=3"}');
     });
   });
 
