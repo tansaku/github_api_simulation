e5c6321475d3ba3701eab1df68f8b293f5e3b44e
Sam Joseph <tansaku@gmail.com>
2015-02-05 14:41:39 +0000

    switching to followers from public_followers

--- test/github_api_test.js
+++ test/github_api_test.js
@@ -10,7 +10,6 @@ describe('Github API Simulation', function(){
 
   it('should return user data in json format', function(){
     casper.then(function(response){
-      //expect(response.headers.get('Content-Type')).to.equal('text/html; charset=utf-8');    
       expect('body').to.have.text('Github API Simulation');
     });
   });
@@ -19,7 +18,7 @@ describe('Github API Simulation', function(){
     casper.thenOpen(host + '/users/tansaku', function(response){
       expect(response.headers.get('Access-Control-Allow-Origin')).to.equal('*');
       expect(response.headers.get('Content-Type')).to.equal('application/json; charset=utf-8');
-      expect('body').to.have.text('{"login":"tansaku", "html_url":"147", "public_repos":"216", "public_followers":"147", "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3"}');
+      expect('body').to.have.text('{"login":"tansaku", "html_url":"147", "public_repos":"216", "followers":"147", "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3"}');
     });
   });
 
@@ -27,7 +26,7 @@ describe('Github API Simulation', function(){
     casper.thenOpen(host + '/users/henrygarner', function(response){
       expect(response.headers.get('Access-Control-Allow-Origin')).to.equal('*');
       expect(response.headers.get('Content-Type')).to.equal('application/json; charset=utf-8');
-      expect('body').to.have.text('{"login":"henrygarner", "html_url":"24", "public_repos":"77", "public_followers":"24", "avatar_url": "https://avatars.githubusercontent.com/u/24540?v=3"}');
+      expect('body').to.have.text('{"login":"henrygarner", "html_url":"24", "public_repos":"77", "followers":"24", "avatar_url": "https://avatars.githubusercontent.com/u/24540?v=3"}');
     });
   });
 
