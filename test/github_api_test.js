5529f7df865aa8e5e903842ec651a5ed30001c8d
Sam Joseph <tansaku@gmail.com>
2015-02-05 07:31:27 +0000

    cannot run casper just via mocha

--- test/github_api_test.js
+++ test/github_api_test.js
@@ -2,7 +2,13 @@ var chai = require('chai');
 var expect = chai.expect;
 
 describe('Github API Simulation', function(){
+  before(function(){
+    casper.start('http://localhost:9999/');
+  });
+
   it('should return user data in json format', function(){
-    expect(2+2).to.equal(4);
+    casper.then(function(){
+      expect('body').to.have.text('Github API Simulation');
+    });
   });
 });
